9777ad2ef3a5e970f86ea6dc3a8903cc6645b109
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-09-24 19:26:59 +0200

    add title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 /* hier muss noch importiert werden */
 
 export const routes: Routes = [
+
+];
+
+
 /*  { path: '', component: deutschComponent },
   { path: 'en', component: englischComponent }*/
-];
