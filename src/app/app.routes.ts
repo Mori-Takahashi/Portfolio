f01d37400e065f1207852af84ca25dd044df56e4
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-09-24 14:57:15 +0200

    add assats

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+/* hier muss noch importiert werden */
 
-export const routes: Routes = [];
+export const routes: Routes = [
+/*  { path: '', component: deutschComponent },
+  { path: 'en', component: englischComponent }*/
+];
