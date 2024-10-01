fa1b36827c15cd4eb445713491c7bc2784cad5b2
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-01 12:11:59 +0200

    styling card and add logic

--- src/app/projecs/projecs.component.ts
+++ src/app/projecs/projecs.component.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './projecs.component.html',
   styleUrl: './projecs.component.scss'
 })
-export class ProjecsComponent {
 
+
+export class ProjecsComponent {
+  procjects: Projecs[] = [{
+    name: 'Join',
+    tecUsed: 'HTML | CSS | JavaScript | Firebase'
+  },
+    {
+      name: 'El Pollo Loco',
+      tecUsed: 'HTML | CSS | JavaScript'
+    }];
+
+  openProject(projectID: number) {
+    console.log(`Open project ${projectID}`);
+  }
+
+}
+
+
+
+type Projecs = {
+  name: string;
+  tecUsed: string;
 }
