ce9f854d3e87bfde00140db6495adc922b0edc93
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-04 14:17:36 +0200

    add dots

--- src/app/review/slider-card/slider-card.component.ts
+++ src/app/review/slider-card/slider-card.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import {ReviewService} from "../../service/review.service";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-slider-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './slider-card.component.html',
   styleUrl: './slider-card.component.scss'
 })
 export class SliderCardComponent {
-  constructor(public reviewService: ReviewService) {
+
+  currentIndex: number = 0;
+
+  constructor(public reviewService: ReviewService) {}
+
+  nextReview() {
+    if (this.currentIndex < this.reviewService.reviews.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
   }
+
+  prevReview() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.reviewService.reviews.length - 1;
+    }
+  }
+
 }
--- src/app/service/review.service.ts
+++ src/app/service/review.service.ts
@@ -7,12 +7,20 @@ export class ReviewService {
 
   reviews = [
     {
-      text: 'Lukas has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project.',
-      author: 'H. Janisch',
+      text: 'Working with Lyonel was always professional, focused and respectful. Lyonel enhanced our Projects with innovative and creative solutions.' +
+        'Notably, his humor consistently create a motivating work atmosphere. Lyonel was a valuable asset to our team, and we look forward to more' +
+        'projects together.',
+      author: 'Philipp Buckstegen',
     },
     {
-      text: 'I had the good fortune of working with Lukas on a project at the Developer Akademie. His effort and calm demeanor always kept our team on track.',
-      author: 'T. Schulz',
+      text: 'Working with Lyonel Berzen was consistently rewarding and enjoyable. Lyonel’s youthful dynamism and talent add substantial value to any' +
+        'project he is involved in. He brings a friendly and encouraging atmosphere to the team, allowing us to excel and deliver our best work. Lyonel' +
+        'remains calm and focused under pressure, always willing to learn new skills and tackle challenges with motivation and discipline. His sympathetic' +
+        'nature and genuine interest in innovation make him a pleasure to work with. With his structured approach and reliability, Lyonel significantly' +
+        'contributed to the success of our projects. He is a team player with a strong design-oriented mindset, ensuring that all tasks are managed' +
+        'efficiently and deadlines are met with high-quality results. Lyonel Berzen is undoubtedly an asset to any team, and he has a bright future ahead' +
+        'of him.',
+      author: 'Tayfun Aya',
     }
   ];
 
