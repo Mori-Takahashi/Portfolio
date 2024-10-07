b37f5d55f0d2b5a92599ce16a0e8ce30257fd603
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-07 11:25:21 +0200

    add contact form component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ContactComponent} from "./contact/contact.component";
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ContactComponent
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
