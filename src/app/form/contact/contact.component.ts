cd0c891478bed09d6ca6993d4119b8eced638f87
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-07 12:31:03 +0200

    add cloudflare turnstile and styling contact area

--- src/app/form/contact/contact.component.ts
+++ src/app/form/contact/contact.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validator, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      'cf-turnstile-response': ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    } else {
+      alert('Please fill out the form before submitting');
+    }
+  }
 
 }
