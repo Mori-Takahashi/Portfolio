89964b7df43419cd304c79005d502681ba8b4b41
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-08 10:46:48 +0200

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+
+/*import * as path from "node:path";*/
 /* hier muss noch importiert werden */
 
 export const routes: Routes = [
-
+  {path: '**', component: PageNotFoundComponent},
+  {path: '404', component: PageNotFoundComponent}
 ];
 
 
-/*  { path: '', component: deutschComponent },
-  { path: 'en', component: englischComponent }*/
+/*  { path: '', component: deutschComponent },*/
