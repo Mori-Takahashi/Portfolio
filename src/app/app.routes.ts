ae4cedc98bee11d6e77d2f1f3b35129d08c0f38d
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-08 10:54:31 +0200

    router test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
+/*import {AboutMeComponent} from "./about-me/about-me.component";
+import {FormComponent} from "./form/form.component";
+import {HeroComponent} from "./hero/hero.component";
+import {ProjecsComponent} from "./projecs/projecs.component";
+import {ReviewComponent} from "./review/review.component";
+import {SkillSetComponent} from "./skill-set/skill-set.component";*/
+
 /*import * as path from "node:path";*/
 /* hier muss noch importiert werden */
 
@@ -11,3 +18,14 @@ export const routes: Routes = [
 
 
 /*  { path: '', component: deutschComponent },*/
+
+/*
+
+  {path: '', component: HeroComponent},
+  {path: '', component: AboutMeComponent},
+  {path: '', component: ProjecsComponent},
+  {path: '', component: SkillSetComponent},
+  {path: '', component: ReviewComponent},
+  {path: '', component: FormComponent},
+
+*/
