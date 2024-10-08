f0dc6826dc86d77027830a54ea3db35ca61e28cf
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-08 11:38:25 +0200

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,14 @@
 import { Routes } from '@angular/router';
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-/*import {AboutMeComponent} from "./about-me/about-me.component";
+import {AboutMeComponent} from "./about-me/about-me.component";
 import {FormComponent} from "./form/form.component";
 import {HeroComponent} from "./hero/hero.component";
 import {ProjecsComponent} from "./projecs/projecs.component";
 import {ReviewComponent} from "./review/review.component";
-import {SkillSetComponent} from "./skill-set/skill-set.component";*/
-
-/*import * as path from "node:path";*/
-/* hier muss noch importiert werden */
+import {SkillSetComponent} from "./skill-set/skill-set.component";
 
 export const routes: Routes = [
-  {path: '**', component: PageNotFoundComponent},
-  {path: '404', component: PageNotFoundComponent}
+
 ];
 
 
