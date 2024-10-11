e95a83fd4a62e1a86f6ec4842722b7770ae8b1ca
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-11 16:05:52 +0200

    add legal-notice.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {SuccessComponent} from "./success/success.component";
+import {LegalNoticeComponent} from "./legal-notice/legal-notice.component";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'home', component: HomeComponent},
   {path: 'success', component: SuccessComponent},
+  {path: 'legalNotice', component: LegalNoticeComponent},
   {path: '404', component: PageNotFoundComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
