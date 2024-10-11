8049ba3248944f4ae8a6824285d29ea053bf562e
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-11 16:36:16 +0200

    styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {HomeComponent} from "./home/home.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {SuccessComponent} from "./success/success.component";
 import {LegalNoticeComponent} from "./legal-notice/legal-notice.component";
+import {PrivacyPolicyComponent} from "./privacy-policy/privacy-policy.component";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'home', component: HomeComponent},
   {path: 'success', component: SuccessComponent},
   {path: 'legalNotice', component: LegalNoticeComponent},
+  {path: 'privacyPolicy', component: PrivacyPolicyComponent},
   {path: '404', component: PageNotFoundComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
