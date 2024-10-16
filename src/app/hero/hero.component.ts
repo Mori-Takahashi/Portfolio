3f8a9302417330490e9bd2b6064d1db12ae42d40
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-10-16 09:33:17 +0200

    add animations

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -14,7 +14,7 @@ export class HeroComponent implements OnInit{
 
   ngOnInit() {
     this.updateEmailInnerHtml();
-
+    this.showButtons();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -33,4 +33,21 @@ updateEmailInnerHtml() {
   }
 }
 
+showButtons() {
+  const buttons = document.getElementById('button1');
+  const buttons2 = document.getElementById('button2');
+  setTimeout(() => {
+    if (buttons && buttons2) {
+      buttons.classList.add('a-link-button');
+      buttons2.classList.add('a-link-button');
+      buttons.classList.add('scale-in-hor-center');
+      buttons2.classList.add('scale-in-hor-center');
+      buttons.classList.remove('hidden');
+      buttons2.classList.remove('hidden');
+    }
+  },);
+}
+/*TODO muss zuende gebaut werden*/
+
+/*a-link-button*/
 }
